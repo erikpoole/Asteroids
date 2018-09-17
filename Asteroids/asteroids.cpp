d26568d6a7c1dabfef3bb3893d6db56bcc57c5e1
Ben Trueman <ctwtrueman@gmail.com>
2018-09-17 12:32:40 -0600

    phase 1

---
+++ Asteroids/asteroids.cpp
@@ -0,0 +1,57 @@
+
+//
+//  Created by Ben Trueman on 9/17/18.
+//  Copyright © 2018 Ben Trueman. All rights reserved.
+//  Team: Ben Trueman & Erik Poole
+
+
+/*
+ class Asteroid{
+ private:
+    int size;
+ 
+ public:
+    constructor
+ 
+ }
+ 
+ class Ship{
+ private:
+    int health
+    int size;
+ 
+ public:
+    constructor
+    destructor/destroy ship
+ 
+ }
+ 
+ class Laser{
+ private:
+    int size;
+ 
+ public:
+    constructor
+ 
+ }
+ 
+ class Collisions{
+ private:
+ 
+ public:
+ 
+ }
+ 
+ Your plan should include at least the following information:
+ What methods will they need?
+ What data will they need to store?
+ In what order will you implement them?
+ What tests will you write along the way to verify that you're making progress?
+
+ 
+ 
+*/
+
+int main(int argc, const char * argv[]) {
+
+}
